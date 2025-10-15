a06891be84512be1069ddc533ba5b89ac5894277
lilian <li4bou@gmail.com>
2025-10-15 12:35:38 +0200

    full template

---
+++ src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug = True, host = '127.0.0.1', port = 5000)
